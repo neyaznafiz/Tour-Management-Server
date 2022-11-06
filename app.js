b391ac0cbfe0852d1f6d2301934bed47198d54ee
neyaznafiz <niiyazobug@gmail.com>
2022-11-07 01:01:32 +0600

    404 route not found impliment

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ const mongoose = require("mongoose");
 const { reset } = require("nodemon");
 const tourRoute = require("./routes/tour.route");
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Route
+app.use("/api/v1/tour-info", tourRoute);
+
+// Default Route
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
 
-app.use("/api/v1/tour-info", tourRoute);
-
-// app.post("/api/v1/tour-info", (req, res, next) => {
-//     // res.send("post is working");
-//     res.send(req.body)
-// });
+// Not Found Route
+app.get("*", (req, res) => {
+  res.status(404).send({
+    status: 404,
+    message: "Route Not Found"
+  });
+});
 
 module.exports = app;
